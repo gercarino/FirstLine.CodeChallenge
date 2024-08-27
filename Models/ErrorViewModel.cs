c1a68dccd4bdb830c354d7ae3d69f3dbbb54cedd
Germain Carino <germain.carino@outlook.com>
2024-08-26 21:12:30 -0600

    Add project files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FirstLine.CodeChallenge.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
